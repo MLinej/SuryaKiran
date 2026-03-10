7df744efd2af19845ef5bf7db65962c2df702e37
Nirbhay71 <nirbhayshingala71@gmail.com>
2026-03-10 21:47:17 +0530

    fix: revert to standard Prisma client without driver adapters

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,15 +1,9 @@
 const { PrismaClient } = require('@prisma/client');
-const { Pool } = require('pg');
-const { PrismaPg } = require('@prisma/adapter-pg');
 
 let prisma;
 
 if (!prisma) {
-  const connectionString = process.env.DATABASE_URL;
-  const pool = new Pool({ connectionString });
-  const adapter = new PrismaPg(pool);
-
-  prisma = new PrismaClient({ adapter });
+  prisma = new PrismaClient();
 }
 
 module.exports = prisma;
